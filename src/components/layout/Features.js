ae7f0ef070aa6cda0bbb4e56ba071a8fe17e96b9
David Erickson <daviderickson@Davids-MacBook-Pro.local>
2020-08-16 10:57:29 -0400

    Created Feature component and styled the Features component to display grid. Also installed react-inlinesvg

--- src/components/layout/Features.js
+++ src/components/layout/Features.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import SVG from 'react-inlinesvg';
 import styled from 'styled-components'
 import { colors } from '../../styles/mainStyles'
 
+import Feature from '../widgets/Feature'
+
 const FeaturesWrapper = styled.div`
-    background-color: ${colors.GreyLight02};
     grid-column: center-start / center-end;
+
+    margin: 15rem, 0;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 6rem;
 `
 
 export class Features extends Component {
     render() {
         return (
             <FeaturesWrapper>
-                {this.props.children}
+                <Feature>Feature 1</Feature>
+                <Feature>Feature 2</Feature>
+                <Feature>Feature 3</Feature>
+                <Feature>Feature 4</Feature>
+                <Feature>Feature 5</Feature>
+                <Feature>Feature 6</Feature>
             </FeaturesWrapper>
         )
     }
